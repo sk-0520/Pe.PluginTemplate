1c2a60278e0a1be1c65dd8ab6500e8e8d8879fc5
sk-0520 <sk-0520@users.noreply.github.com>
2024-10-18 09:54:12 +0000

    initialize Test.Plugin

---
+++ Source/Test.Plugin.Test/Sample.Test.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentTypeTextNet.Pe.Bridge.Plugin;
+using ContentTypeTextNet.Pe.Bridge.Plugin.Addon;
+using ContentTypeTextNet.Pe.Bridge.Plugin.Preferences;
+using ContentTypeTextNet.Pe.Embedded.Abstract;
+using Xunit;
+
+namespace Test.Plugin.Test
+{
+    public class SampleTest
+    {
+        #region function
+
+        [Fact]
+        public void SimpleTest()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(4, 2)]
+        [InlineData(6, 3)]
+        [InlineData(8, 4)]
+        public void ParameterTest(int excepted, int input)
+        {
+            Assert.Equal(excepted, input + input);
+        }
+
+        #endregion
+    }
+}
