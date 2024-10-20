52cc4eaeefd5e22cc1c9258febfb6d63711db4f6
sk-0520 <sk-0520@users.noreply.github.com>
2024-10-20 12:08:27 +0000

    initialize Test.Plugin

---
+++ FullTest.ps1
@@ -0,0 +1,17 @@
+Param(
+	[string[]] $Platforms = @('x64', 'x86')
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+$pluginName = 'Test.Plugin'
+
+$scriptDirPath = Join-Path -Path $PSScriptRoot -ChildPath 'Build'
+
+$scripts = @{
+	test = Join-Path -Path $scriptDirPath -ChildPath 'test-project.ps1'
+}
+
+Write-Information 'プロジェクトテスト'
+& $scripts.test -ProjectName "$pluginName.Test" -Platforms $Platforms
