cba12947cdd73737a85f121d311a0f2e13e81a7c
sk-0520 <sk-0520@users.noreply.github.com>
2026-09-12 07:43:46 +0000

    initialize Test.Plugin

---
+++ Source/Test.Plugin/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentTypeTextNet.Pe.Bridge.Plugin;
+using ContentTypeTextNet.Pe.Embedded.Attributes;
+
+[assembly: PluginIdentifiers("Test.Plugin", "00000000-0000-0000-0000-000000000000")]
+[assembly: PluginSupportVersions("0.0.0", "0.0.0", "https://example.com/update-check.json")]
+[assembly: PluginAuthors("<author>", PluginLicense.Unknown, "<website>", "<projectsite>", "<email>")]
